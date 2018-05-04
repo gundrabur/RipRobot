e13d624f961e1f88463a2c5661ed4f1d6e9c61a1
gundrabur <38981012+gundrabur@users.noreply.github.com>
2018-05-04 15:55:47 +0200

    Update moveAudio.sh

--- moveAudio.sh
+++ moveAudio.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "AUDIO AutoRipper: Dateien werden auf USB-Stick verschoben"
-sudo rsync --progress --modify-window=1 --update --recursive --times --ignore-existing /home/pi/AutoRipper/tmp/ /media/usb/ >/home/pi/AutoRipper/rsync.log
-#sudo rm -r /home/pi/AutoRipper/tmp/*
-echo "AUDIO AutoRipper: Dateien wurden auf USB-Stick verschoben"
+echo "AUDIO RipRobot: Dateien werden auf USB-Stick verschoben"
+sudo rsync --progress --modify-window=1 --update --recursive --times --ignore-existing /home/pi/RipRobot/tmp/ /media/usb/ >/home/pi/RipRobot/rsync.log
+echo "AUDIO RipRobot: Dateien wurden auf USB-Stick verschoben"
