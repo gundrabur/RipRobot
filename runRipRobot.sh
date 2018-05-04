4380e6be05aaaa12ca0045e6db9b271a3fed53bd
gundrabur <38981012+gundrabur@users.noreply.github.com>
2018-05-04 16:02:57 +0200

    Update runRipRobot.sh

--- runRipRobot.sh
+++ runRipRobot.sh
@@ -2,5 +2,5 @@
 echo "AUDIO RipRobot: Alle Temporäre Dateien werden vor dem Start gelöscht"
 sudo rm -r /home/pi/RipRobot/tmp/*
 cd /home/pi/RipRobot
-python ripRobot.py /home/pi/RipRobot/tmp 100
+python ripRobot.py /home/pi/RipRobot/tmp 180
 echo "AUDIO RipRobot: System wird abgeschaltet!"
